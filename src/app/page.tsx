2aac71adb14dc8ff27216c143cf6d941cad3a5b1
Australia Server <australiaserver150@gmail.com>
2025-11-27 21:18:58 +0000

    Display skeleton loading cards with shimmer animation while products are

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,12 +58,18 @@ export default function Home() {
       <section ref={productsRef}>
         {loading ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            {Array.from({ length: 4 }).map((_, i) => (
-                <div key={i} className="flex flex-col space-y-3">
-                    <Skeleton className="h-[200px] w-full rounded-xl" />
+            {Array.from({ length: 8 }).map((_, i) => (
+                <div key={i} className="flex flex-col space-y-3 rounded-lg border border-primary/20 p-4">
+                    <Skeleton className="h-48 w-full rounded-lg" />
                     <div className="space-y-2">
-                        <Skeleton className="h-4 w-[250px]" />
-                        <Skeleton className="h-4 w-[200px]" />
+                        <Skeleton className="h-6 w-3/4" />
+                        <Skeleton className="h-4 w-1/4" />
+                        <Skeleton className="h-4 w-full" />
+                        <Skeleton className="h-4 w-5/6" />
+                    </div>
+                     <div className="flex justify-between items-center pt-2">
+                        <Skeleton className="h-8 w-1/3" />
+                        <Skeleton className="h-10 w-2/5" />
                     </div>
                 </div>
             ))}
